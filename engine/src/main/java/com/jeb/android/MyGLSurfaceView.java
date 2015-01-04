da1b0ce6c7f3a3e06ae58ba95b1d506d35dde47c
Jakub Petriska <jakub.petriska@eman.cz>
2015-01-04 01:52:12 +0100

    unnecessary comment removed

--- engine/src/main/java/com/jeb/android/MyGLSurfaceView.java
+++ engine/src/main/java/com/jeb/android/MyGLSurfaceView.java
@@ -19,11 +19,6 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.view.MotionEvent;
 
-/**
- * A view container where OpenGL ES graphics can be drawn on screen.
- * This view can also be used to capture touch events, such as a user
- * interacting with drawn objects.
- */
 public class MyGLSurfaceView extends GLSurfaceView {
 
     public MyGLSurfaceView(Context context) {
