d765b19ba84e64ce2f8fc8daccec8be5443dd637
Jakub Petriska <jakub.petriska@eman.cz>
2015-02-26 11:10:53 +0100

    TODO removed, Component and GameObject manipulation test class added

---
+++ engine/src/test/java/com/onion/tests/component_gameobject_manipulation/ComponentGameObjectManipulationTest.java
@@ -0,0 +1,9 @@
+package com.onion.tests.component_gameobject_manipulation;
+
+import com.onion.tests.support.BaseEngineTest;
+
+/**
+ * Created by Jakub Petriska on 26. 2. 2015.
+ */
+public class ComponentGameObjectManipulationTest extends BaseEngineTest {
+}
