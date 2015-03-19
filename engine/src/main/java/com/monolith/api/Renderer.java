9c50cd49d9fd363dd07a0b11b983612d88d50343
Jakub Petriska <jakub.petriska@eman.cz>
2015-03-20 00:24:00 +0100

    renderer is no longer a System

--- engine/src/main/java/com/monolith/api/Renderer.java
+++ engine/src/main/java/com/monolith/api/Renderer.java
@@ -1,15 +1,15 @@
 package com.monolith.api;
 
 import com.monolith.api.components.Model;
-import com.monolith.engine.ISystem;
 
 /**
  * Provides rendering functionality.
  */
-public interface Renderer extends ISystem {
+public interface Renderer {
 
     /**
      * Renders model on the position of it's owning {@link com.monolith.api.GameObject}.
+     *
      * @param model Model to render.
      */
     void render(Model model);
@@ -18,7 +18,7 @@ public interface Renderer extends ISystem {
      * Creates instance of MeshData with supplied data.
      * Implementation can return a subclass of MeshData to which it can store it's specific
      * data to use during rendering.
-     *
+     * <p/>
      * Input parameters are similar to {@link com.monolith.api.MeshData}'s fields.
      */
     MeshData createMeshData(
