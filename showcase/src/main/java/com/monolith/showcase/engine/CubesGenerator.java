55b089d99a412c98497c86cfa8eca1bc70d6a08a
Jakub Petriska <jakub.petriska@eman.cz>
2015-04-18 23:13:46 +0200

    second objetc added to performance test

--- showcase/src/main/java/com/monolith/showcase/engine/CubesGenerator.java
+++ showcase/src/main/java/com/monolith/showcase/engine/CubesGenerator.java
@@ -16,22 +16,27 @@ public class CubesGenerator extends Component {
 
     @Override
     public void start() {
+        boolean generateCube = false;
         for (int layer = 0; layer < cubeLayerCount; ++layer) {
             float distance = (layer + 1) * CUBE_SPACE;
             int halfLayerSizeFloor = 1 + layer;
             for (int i = -halfLayerSizeFloor; i <= halfLayerSizeFloor; ++i) {
                 for (int j = -halfLayerSizeFloor; j <= halfLayerSizeFloor; ++j) {
-                    addCube(i * CUBE_SPACE, distance, j * CUBE_SPACE);
-                    addCube(i * CUBE_SPACE, -distance, j * CUBE_SPACE);
+                    String mesh = generateCube ? Primitives.CUBE : "models/diamond.obj";
+                    float scale = generateCube ? 1 : 0.5f;
+                    addCube(i * CUBE_SPACE, distance, j * CUBE_SPACE, mesh, scale);
+                    addCube(i * CUBE_SPACE, -distance, j * CUBE_SPACE, mesh, scale);
+                    generateCube = !generateCube;
                 }
             }
         }
     }
 
-    private void addCube(float x, float y, float z) {
+    private void addCube(float x, float y, float z, String mesh, float scale) {
         GameObject newCube = new GameObject(getGameObject());
+        newCube.transform.setScale(scale, scale, scale);
         Model model = new Model();
-        model.meshPath = Primitives.CUBE;
+        model.meshPath = mesh;
         newCube.addComponent(model);
         newCube.transform.setPosition(x, y, z);
     }
