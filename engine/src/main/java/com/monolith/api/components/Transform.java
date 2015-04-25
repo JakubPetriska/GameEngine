8a4f4f5bc1cb60dc0ec6803bbf60aa2d54cbc9f9
Jakub Petriska <jakub.petriska@eman.cz>
2015-04-25 15:50:36 +0200

    Transform documentation finished

--- engine/src/main/java/com/monolith/api/components/Transform.java
+++ engine/src/main/java/com/monolith/api/components/Transform.java
@@ -7,12 +7,11 @@ import com.monolith.api.math.Vector3;
 
 import java.util.List;
 
-// TODO comment this class
 /**
- * Holds transformation data of a {@link com.monolith.api.GameObject}.
+ * Holds transformation of a {@link com.monolith.api.GameObject}.
  * <p/>
- * Transformation is relative to parent GameObject. World (absolute) transformation
- * data can be obtained using appropriate methods.
+ * Transformation is relative to parent GameObject.
+ * World (absolute) transformation can be obtained using appropriate methods.
  */
 public class Transform extends Component {
 
@@ -38,51 +37,106 @@ public class Transform extends Component {
      */
     private final Vector3 mScale = new Vector3();
 
+    /**
+     * Create new transformation.
+     * <p/>
+     * Newly created transformation is identity. It has 0 translation,
+     * 0 rotation and scale of 1 in all axes.
+     */
     public Transform() {
         mScale.set(1, 1, 1);
     }
 
+    /**
+     * Get the local space X position.
+     *
+     * @return The local space X position.
+     */
     public float getPositionX() {
         return mPosition.getX();
     }
 
+    /**
+     * Get the local space Y position.
+     *
+     * @return The local space Z position.
+     */
     public float getPositionY() {
         return mPosition.getY();
     }
 
+    /**
+     * Get the local space Z position.
+     *
+     * @return The local space Z position.
+     */
     public float getPositionZ() {
         return mPosition.getZ();
     }
 
+    /**
+     * Get the local space rotation around X axis.
+     *
+     * @return The local space rotation around X axis.
+     */
     public float getRotationX() {
         return mRotation.getX();
     }
 
+    /**
+     * Get the local space rotation around Y axis.
+     *
+     * @return The local space rotation around Y axis.
+     */
     public float getRotationY() {
         return mRotation.getY();
     }
 
+    /**
+     * Get the local space rotation around Z axis.
+     *
+     * @return The local space rotation around Z axis.
+     */
     public float getRotationZ() {
         return mRotation.getZ();
     }
 
+    /**
+     * Get the local space scale in direction of X axis.
+     *
+     * @return The local space scale in direction of X axis.
+     */
     public float getScaleX() {
         return mScale.getX();
     }
 
+    /**
+     * Get the local space scale in direction of Y axis.
+     *
+     * @return The local space scale in direction of Y axis.
+     */
     public float getScaleY() {
         return mScale.getY();
     }
 
+    /**
+     * Get the local space scale in direction of Z axis.
+     *
+     * @return The local space scale in direction of Z axis.
+     */
     public float getScaleZ() {
         return mScale.getZ();
     }
 
     /**
-     * Translation is done relative to the already present transform. TODO finish this comment
-     * @param x
-     * @param y
-     * @param z
+     * Translate this transform by given amount in direction of each axis.
+     * <p/>
+     * The axes along which the transformation is translated are
+     * transformed by this transformation.
+     *
+     * @param x Amount of translation in the direction of X axis.
+     * @param y Amount of translation in the direction of Y axis.
+     * @param z Amount of translation in the direction of Z axis.
      */
     public void translateBy(float x, float y, float z) {
         sWorkVector.set(x, y, z);
@@ -94,6 +148,13 @@ public class Transform extends Component {
         invalidate();
     }
 
+    /**
+     * Set the position (translation from origin) of this transformation.
+     *
+     * @param x X coordinate of position.
+     * @param y Y coordinate of position.
+     * @param z Z coordinate of position.
+     */
     public void setPosition(float x, float y, float z) {
         mPosition.set(x, y, z);
         invalidate();
@@ -101,16 +162,37 @@ public class Transform extends Component {
 
     // TODO adjust the rotation so it is in range 0 - 360
 
+    /**
+     * Rotate this transformation by given amounts in degrees.
+     *
+     * @param x Amount of rotation around X axis.
+     * @param y Amount of rotation around Y axis.
+     * @param z Amount of rotation around Z axis.
+     */
     public void rotateBy(float x, float y, float z) {
         mRotation.add(x, y, z);
         invalidate();
     }
 
+    /**
+     * Set the rotation of this transformation.
+     *
+     * @param x Amount of rotation around X axis.
+     * @param y Amount of rotation around Y axis.
+     * @param z Amount of rotation around Z axis.
+     */
     public void setRotation(float x, float y, float z) {
         mRotation.set(x, y, z);
         invalidate();
     }
 
+    /**
+     * Scale this transformation by given amounts.
+     *
+     * @param x Amount of scale in direction of X axis.
+     * @param y Amount of scale in direction of Y axis.
+     * @param z Amount of scale in direction of Z axis.
+     */
     public void scaleBy(float x, float y, float z) {
         mScale.setX(mScale.getX() * x);
         mScale.setY(mScale.getY() * y);
@@ -118,17 +200,24 @@ public class Transform extends Component {
         invalidate();
     }
 
+    /**
+     * Set scale of this transformation.
+     *
+     * @param x Amount of scale in direction of X axis.
+     * @param y Amount of scale in direction of Y axis.
+     * @param z Amount of scale in direction of Z axis.
+     */
     public void setScale(float x, float y, float z) {
         mScale.set(x, y, z);
         invalidate();
     }
 
     private void invalidate() {
-        if(mTransformationMatrixValid) {
+        if (mTransformationMatrixValid) {
             mTransformationMatrixValid = false;
 
             List<GameObject> children = getGameObject().children;
-            for(int i = 0; i < children.size(); ++i) {
+            for (int i = 0; i < children.size(); ++i) {
                 children.get(i).transform.invalidate();
             }
         }
@@ -138,8 +227,16 @@ public class Transform extends Component {
     private final Matrix44 mHelperMatrix = new Matrix44();
 
     // TODO If anybody changes this matrix but not through the transformation rendering goes nuts
+
+    /**
+     * Get the transformation matrix.
+     * <p/>
+     * Transformation matrix is absolute. Meaning the transformation is from world's origin.
+     *
+     * @return The absolute transformation matrix.
+     */
     public Matrix44 getTransformationMatrix() {
-        if(!mTransformationMatrixValid) {
+        if (!mTransformationMatrixValid) {
             GameObject parent = getGameObject().getParent();
             boolean hasParent = parent != null;
             Matrix44 parentTransformation = hasParent ? parent.transform.getTransformationMatrix() : null;
@@ -152,7 +249,7 @@ public class Transform extends Component {
             localTransformation.rotateY(mRotation.getY());
             localTransformation.translate(mPosition);
 
-            if(hasParent) {
+            if (hasParent) {
                 Matrix44.multiply(mTransformationMatrix, parentTransformation, localTransformation);
             }
             mTransformationMatrixValid = true;
