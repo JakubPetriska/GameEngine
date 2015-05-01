0cda916f0b6bd90940eb0ccebdc1c4074ec8e7fd
Jakub Petriska <jakub.petriska@eman.cz>
2015-05-01 18:15:02 +0200

    Application documentation edited

--- engine/src/main/java/com/monolith/api/Application.java
+++ engine/src/main/java/com/monolith/api/Application.java
@@ -5,7 +5,7 @@ import com.monolith.engine.MeshManager;
 /**
  * Provides important engine features. Instance of this class is passed everywhere around the
  * engine.
- *
+ * <p/>
  * Contains global application functionality, such as scene changing and
  * other more specific objects containing the specific data or functionality.
  */
@@ -25,6 +25,13 @@ public abstract class Application {
 
     public abstract Debug getDebug();
 
+    /**
+     * Keep in mind that this method can return different instance in different frames.
+     * And therefore result of this method should not be cached.
+     * This can happen due to change in screen orientation.
+     *
+     * @return Instance of Display relevant for current frame.
+     */
     public abstract Display getDisplay();
 
     public abstract void changeScene(String newSceneName);
